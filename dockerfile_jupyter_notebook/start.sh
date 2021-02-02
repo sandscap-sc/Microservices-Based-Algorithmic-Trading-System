a801f282d3ee1d646df0a0f72e1e8e7fd8829d23
bmello4688 <brian.mello@outlook.com>
2021-02-01 22:14:41 -0500

    fixed docker issues

---
+++ dockerfile_jupyter_notebook/start.sh
@@ -0,0 +1,5 @@
+python /home/jovyan/work/q_pack/db_pack/schema/secmaster_db_schema_builder.py
+python /home/jovyan/work/q_pack/db_pack/schema/secmaster_db_symbol_loader.py
+python /home/jovyan/work/q_pack/db_pack/schema/risk_db_schema_builder.py
+
+jupyter notebook --no-browser --NotebookApp.token='' --NotebookApp.password='' --allow-root
